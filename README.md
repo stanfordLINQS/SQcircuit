e7dcb595b047661d472b2ace75cd01abc98fbb55
Taha <tahar@stanford.edu>
2022-03-03 00:52:39 -0800

    setup.py added

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 <img src = pics/README_logo.png width= 250px" />
 </p>
 
-# <span style="color:Orange">*SQ*</span>circuit: superconducting quantum circuit analyzer
+# SQcircuit: superconducting quantum circuit analyzer
 [**What is SQcircuit?**](#What-is-SQcircuit?)
 |[**Installation**](#Installation)
 |[**Documentation**](#Documentation)
@@ -108,13 +108,16 @@ super conducting circuits of the literature, in which we effortlessly reproduce 
 by SQcircuit functionalities. One can find the jupyter notebook examples in example folder, some of which are:
 
 * [Zero-Pi Qubit](https://github.com/stanfordLINQS/SQcircuit/blob/main/examples/zeroPiQubit.ipynb): We calculated the
-energy spectrum and eigenfunctions of the zero-pi qubit in the \cite{}
+energy spectrum and eigenfunctions of the zero-pi qubit in the 
+[Groszkowski2018](https://iopscience-iop-org.stanford.idm.oclc.org/article/10.1088/1367-2630/aab7cd)
 * [Inductively Shunted Circuit](https://github.com/stanfordLINQS/SQcircuit/blob/main/examples/inductivelyShunted.ipynb):
-In \cite{}, they explained how the conventional method or perturbation theory does not correctly diagonalize their 
-highly anharmonic inductively-shunted qubits. However, using SQcircuit, we simply reproduced the energy spectrum.
+[Smith2016](https://journals-aps-org.stanford.idm.oclc.org/prb/abstract/10.1103/PhysRevB.94.144507)
+explained how the conventional method or perturbation theory does not correctly diagonalize their 
+highly anharmonic inductively-shunted qubits. However, by using SQcircuit, we simply reproduced the energy spectrum.
 * [Qubit protected by two Cooper-pair tunneling](https://github.com/stanfordLINQS/SQcircuit/blob/main/examples/twoCPB.ipynb):
-\cite{} designed a qubit that is protected by two Cooper-pair tunneling. We reproduced the main results of the paper 
-such as energy spectrum, wavefunctions, and matrix elements.
+[Smith2020](https://doi-org.stanford.idm.oclc.org/10.1038/s41534-019-0231-2)
+designed a qubit that is protected by two Cooper-pair tunneling. We reproduced the main results of the paper such as
+energy spectrum, wavefunctions, and matrix elements.
 
 ## Contribution
 You are very welcome to contribute to SQcircuit development by forking this repository and sending pull requests,
---
+++ setup.py
@@ -0,0 +1,119 @@
+import os
+import sys
+
+import setuptools
+
+DOCLINES = __doc__.split("\n")
+
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: BSD License
+Programming Language :: Python :: 3
+Topic :: Scientific/Engineering
+Intended Audience :: Science/Research
+Operating System :: OS Independent
+"""
+
+
+EXTRA_KWARGS = {}
+
+# version information about scqubits goes here
+MAJOR = 2
+MINOR = 2
+MICRO = 2
+ISRELEASED = True
+
+VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(CURRENT_DIR, "requirements.txt")) as requirements:
+    INSTALL_REQUIRES = requirements.read().splitlines()
+
+
+EXTRAS_REQUIRE = {
+    "graphics": ["matplotlib-label-lines (>=0.3.6)"],
+    "explorer": ["ipywidgets (>=7.5)"],
+    "h5-support": ["h5py (>=2.10)"],
+    "pathos": ["pathos", "dill"],
+    "fitting": ["lmfit"],
+}
+
+
+DESCRIPTION = DOCLINES[0]
+LONG_DESCRIPTION = "\n".join(DOCLINES[2:])
+CLASSIFIERS = [_f for _f in CLASSIFIERS.split("\n") if _f]
+
+
+def git_short_hash():
+    try:
+        git_str = "+" + os.popen('git log -1 --format="%h"').read().strip()
+    except OSError:
+        git_str = ""
+    else:
+        if git_str == "+":  # fixes setuptools PEP issues with versioning
+            git_str = ""
+    return git_str
+
+
+FULLVERSION = VERSION
+if not ISRELEASED:
+    FULLVERSION += ".dev" + str(MICRO) + git_short_hash()
+
+
+def write_version_py(filename="scqubits/version.py"):
+    cnt = """\
+# THIS FILE IS GENERATED FROM scqubits SETUP.PY
+short_version = '%(version)s'
+version = '%(fullversion)s'
+release = %(isrelease)s
+"""
+    versionfile = open(filename, "w")
+    try:
+        versionfile.write(
+            cnt
+            % {
+                "version": VERSION,
+                "fullversion": FULLVERSION,
+                "isrelease": str(ISRELEASED),
+            }
+        )
+    finally:
+        versionfile.close()
+
+
+local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+os.chdir(local_path)
+sys.path.insert(0, local_path)
+sys.path.insert(0, os.path.join(local_path, "scqubits"))  # to retrieve _version
+
+# always rewrite _version
+if os.path.exists("scqubits/version.py"):
+    os.remove("scqubits/version.py")
+write_version_py()
+
+setuptools.setup(
+    name="SQcircuit",
+    version=FULLVERSION,
+    author="Taha Rajabzadeh, Amir Safavi-Naeini",
+    author_email="tahar@stanford.edu, safavi@stanford.edu",
+    license="BSD",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    keywords="superconducting circuits",
+    url="https://github.com/stanfordLINQS/SQcircuit",
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    zip_safe=False,
+    include_package_data=True,
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+    ]
+)
