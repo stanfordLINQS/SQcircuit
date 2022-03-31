584164ffec54bd5063d3b3694d6582a8c6b1b119
Taha <tahar@stanford.edu>
2022-03-31 11:13:29 -0700

    testing  workflows

--- SQcircuit/tests/test_qubits.py
+++ SQcircuit/tests/test_qubits.py
@@ -5,13 +5,13 @@ test_qubits contains the test cases for well-known qubits.
 from SQcircuit.tests.conftest import QubitTest
 
 
-class TestZeroPi(QubitTest):
-    """
-    class for testing the zero-pi qubit
-    """
-    @classmethod
-    def setup_class(cls):
-        cls.fileName = "zeroPi_1"
+# class TestZeroPi(QubitTest):
+#     """
+#     class for testing the zero-pi qubit
+#     """
+#     @classmethod
+#     def setup_class(cls):
+#         cls.fileName = "zeroPi_1"
 
 
 class TestInductivelyShunted(QubitTest):
@@ -23,13 +23,13 @@ class TestInductivelyShunted(QubitTest):
         cls.fileName = "inductivelyShunted_1"
 
 
-# class TestFluxonium(QubitTest):
-#     """
-#     class for testing the zero-pi qubit
-#     """
-#     @classmethod
-#     def setup_class(cls):
-#         cls.fileName = "Fluxonium_1"
+class TestFluxonium(QubitTest):
+    """
+    class for testing the zero-pi qubit
+    """
+    @classmethod
+    def setup_class(cls):
+        cls.fileName = "Fluxonium_1"
 
 
 class TestFluxonium2(QubitTest):
