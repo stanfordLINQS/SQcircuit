107c1bf33770563dd1441d659bdd844f49125e72
Taha <tahar@stanford.edu>
2022-04-09 13:04:18 -0700

    update setup.py for new releas

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ DESCRIPTION = "superconducting quantum circuit analyzer"
 # version of the SQcircuit
 MAJOR = 0
 MINOR = 0
-PATCH = 4
+PATCH = 5
 ISRELEASED = True
 
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, PATCH)
