bbfc2aeb83ca92f4e21e44d4669807cece9fc6cd
Alekxos <mckeehan@stanford.edu>
2022-08-19 21:27:27 -0700

    Update utils_backup.py with some bug fixes

--- SQcircuit/utils_backup.py
+++ SQcircuit/utils_backup.py
@@ -7,9 +7,10 @@ from typing import List
 from SQcircuit.circuit import Circuit
 
 import torch
+import numpy as np
 
 
-def vectorize(circuit: Circuit) -> List[torch.Tensor]:
+def _vectorize(circuit: Circuit) -> torch.Tensor:
     """Converts a circuit to an ordered set of PyTorch tensors, corresponding
     to the values of each element in the same order.
 
@@ -18,12 +19,12 @@ def vectorize(circuit: Circuit) -> List[torch.Tensor]:
         circuit:
             A circuit to vectorize.
     """
-    elements = circuit.elements.values()
+    elements = list(circuit.elements.values())[0]
     element_values = [element.get_value(element_units=True) for element in elements]
     element_tensors = [torch.as_tensor(element_value)
                        for element_value in element_values]
-    return torch.stack(element_tensors)
-
+    element_tensors = torch.stack(element_tensors)
+    return element_tensors
 
 def eigencircuit(circuit: Circuit, num_eigen):
     """Solves for the eigenvalues of a circuit, returning them
@@ -34,31 +35,36 @@ def eigencircuit(circuit: Circuit, num_eigen):
         circuit:
             A circuit for which the eigensystem will be solved.
     """
-    element_tensors = vectorize(circuit)
+
+    elements = list(circuit.elements.values())[0]
+    initial_element_vals = [element.get_value(element_units=False) for element in elements]
+
+    element_tensors = _vectorize(circuit)
     element_tensors.requires_grad = True
 
     class EigenvalueSolver(torch.autograd.Function):
         @staticmethod
         def forward(ctx, element_tensors):
-            elements = circuit.elements.values()
-            values_units = [(element_tensors[idx].numpy(), element_tensors[idx].unit)
+            elements = list(circuit.elements.values())[0]
+            values_units = [(element_tensors[idx].numpy(), elements[idx].unit)
                             for idx in range(len(element_tensors))]
             circuit.update_elements(elements, values_units=values_units)
             H = circuit.hamiltonian()
-            eigenvalues, _ = H.diag(num_eigen = num_eigen)
+            eigenvalues, _ = circuit.diag(n_eig=num_eigen)
+            eigenvalues = [eigenvalue * 1e9 * 2 * np.pi for eigenvalue in eigenvalues]
             eigenvalue_tensors = [torch.as_tensor(eigenvalue) for eigenvalue in eigenvalues]
             return torch.stack(eigenvalue_tensors)
 
         @staticmethod
         def backward(ctx, grad_output):
-            cr_elements = circuit.elements.values()
+            cr_elements = list(circuit.elements.values())[0]
             m, n = element_tensors.shape[0], num_eigen
-            partial_omega = torch.FloatTensor((m, n))
+            partial_omega = torch.zeros([m, n], dtype=float)
             for el_idx in range(m):
                 for eigen_idx in range(n):
-                    partial_omega[el_idx, eigen_idx] = Circuit.get_partial_omega(el = cr_elements[el_idx],
-                                                              m = eigen_idx)
-
-            return partial_omega @ grad_output
+                    partial_omega[el_idx, eigen_idx] = circuit.get_partial_omega(el=cr_elements[el_idx],
+                                                                                 m=eigen_idx, subtract_ground=True) * \
+                                                       initial_element_vals[el_idx]
+            return torch.sum(partial_omega * grad_output, axis=-1)
 
     return element_tensors, EigenvalueSolver(element_tensors)
