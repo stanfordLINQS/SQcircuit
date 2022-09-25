658c98ba03b42edaea41fe90ecd47080497de07a
Taha <tahar@stanford.edu>
2022-09-24 23:22:14 -0700

    added the logs.py for handling the errors and warnings

---
+++ SQcircuit/logs.py
@@ -0,0 +1,4 @@
+"""errors.py contains the error messages for SQcircuit"""
+
+UNIT_ERROR = "The input unit is not correct. Look at the API documentation " \
+             "for the correct input format."
