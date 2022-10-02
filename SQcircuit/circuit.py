Fix issue with tensor initialization to enable full backprop through transmon circuit

SQcircuit/circuit.py
@@ -1478,7 +1478,7 @@ class Circuit:
 
         return efreqs_sorted / (2 * np.pi * unt.get_unit_freq()), evecs_sorted
 
-    def diag_torch(self, n_eig: int) -> Tuple[Union[ndarray, Tensor], List[Union[Qobj, Tensor]]]:
+    def diag_torch(self, n_eig: int) -> Tuple[Tensor, Tensor, Tensor]:
         print("diag Torch")
         tensor_list, EigenvalueSolver, EigenvectorSolver = sqf.eigencircuit(self, num_eigen = n_eig)
         eigenvalues = EigenvalueSolver.apply(tensor_list)
@@ -1486,7 +1486,7 @@ class Circuit:
         self._efreqs = eigenvalues
         self._evecs = eigenvectors
 
-        return eigenvalues / (2 * np.pi * unt.get_unit_freq()), eigenvectors
+        return tensor_list, eigenvalues / (2 * np.pi * unt.get_unit_freq()), eigenvectors
 
     def diag(self, n_eig: int) -> Tuple[Union[ndarray, Tensor], List[Union[Qobj, Tensor]]]:
         """
@@ -1732,7 +1732,6 @@ class Circuit:
         op = self.coupling_op(ctype, nodes)
 
         # TODO: Check operator format/dtype, compare to using states from numpy solver directly
-
         return sqf.unwrap(sqf.mat_mul(sqf.mat_mul(sqf.dag(state1), op), state2))
 
     @staticmethod

SQcircuit/functions.py
@@ -114,7 +114,7 @@ def init_sparse(shape):
 
 def init_op(size):
     if get_optim_mode():
-        return torch.empty(size = size, dtype = torch.complex128)
+        return torch.zeros(size = size, dtype = torch.complex128)
         # return torch.sparse_coo_tensor(size = size, dtype=torch.complex128)
     return qt.Qobj()
 
@@ -190,7 +190,7 @@ def mat_mul(A, B):
     if get_optim_mode():
         A = dense(A)
         B = dense(B)
-        return torch.matmul(torch.as_tensor(A, dtype=torch.complex128), torch.as_tensor(B, dtype=torch.complex128))
+        return torch.as_tensor(A, dtype=torch.complex128) @ torch.as_tensor(B, dtype=torch.complex128)
     if isinstance(A, qt.Qobj) and isinstance(B, qt.Qobj):
         return A * B
     return A @ B

SQcircuit/tests/test_grad.py
@@ -5,29 +5,84 @@ functionalities.
 
 from SQcircuit.elements import Capacitor, Junction
 from SQcircuit.settings import set_optim_mode
-from SQcircuit.circuit import Circuit
+from SQcircuit.circuit import Circuit, unt
 
-def test_T1_linearization():
+import numpy as np
+
+def test_T1():
     optim = True
     set_optim_mode(optim)
 
-    # Define the circuit elements
+    # Construct the circuit
     cap_unit, ind_unit = 'pF', 'GHz'
     C = Capacitor(7.746, cap_unit, Q=1e6, requires_grad=optim)
     J = Junction(12, ind_unit, requires_grad=optim)
 
-    # Define the circuit
     elements = {
         (0, 1): [C, J],
     }
 
-    # Diagonalize the circuit
     cr_transmon = Circuit(elements)
     cr_transmon.set_trunc_nums([4, ])
-    cr_transmon.diag(2)
+    tensors, _, _ = cr_transmon.diag(3)
     T1_inv = cr_transmon.dec_rate('capacitive', (0, 1))
+    T1_inv_0 = float(T1_inv)
+    print(f"Initial T1^-1 (torch): {T1_inv_0}")
 
+    # Compute gradient with autodifferentiation
     T1_inv.backward()
-    print(f"Grad: {C.get_value().grad}")
-    assert False
+    print(f"Val grad C (torch): {C._value.grad * unt.farad_list[C.unit]}")
+    print(f"Val grad J (torch): {J._value.grad * unt.freq_list[J.unit] / (2 * np.pi)}")
     set_optim_mode(False)
+
+    assert False
+
+def test_T1_linearization():
+    # Compute gradient with linear approximation
+    set_optim_mode(False)
+    cap_unit, ind_unit = 'pF', 'GHz'
+    delta = 1e-8
+    C = Capacitor(7.746, cap_unit, Q=1e6)
+    J = Junction(12, ind_unit)
+    elements = {
+        (0, 1): [C, J],
+    }
+
+    # Default T1 value
+    cr_transmon = Circuit(elements)
+    cr_transmon.set_trunc_nums([4, ])
+    cr_transmon.diag(3)
+    T1_inv = cr_transmon.dec_rate('capacitive', (0, 1))
+    print(f"T1^-1 (numpy): {T1_inv}")
+
+    delta_omega_0 = cr_transmon._efreqs[0]
+    delta_omega_1 = cr_transmon._efreqs[1]
+
+    # Gradient of capacitor
+    C_delta = Capacitor(7.746 + delta, cap_unit, Q=1e6)
+    elements = {
+        (0, 1): [C_delta, J],
+    }
+    cr_transmon = Circuit(elements)
+    cr_transmon.set_trunc_nums([4, ])
+    cr_transmon.diag(3)
+    T1_inv_delta_C = cr_transmon.dec_rate('capacitive', (0, 1))
+    dT1InvdC = (T1_inv_delta_C - T1_inv) / delta
+    print(f"d(T1^-1)/dC: {dT1InvdC}")
+
+    delta_omega_0_prime = cr_transmon._efreqs[0]
+    delta_omega_1_prime = cr_transmon._efreqs[1]
+
+    # Gradient of junction
+    J_delta = Junction(12 + delta, ind_unit)
+    elements = {
+        (0, 1): [C, J_delta],
+    }
+    cr_transmon = Circuit(elements)
+    cr_transmon.set_trunc_nums([4, ])
+    cr_transmon.diag(3)
+    T1_inv_delta_J = cr_transmon.dec_rate('capacitive', (0, 1))
+    dT1InvdEJ = (T1_inv_delta_J - T1_inv) / delta
+    print(f"d(T1^-1)/dEJ: {dT1InvdEJ}")
+
+    assert False