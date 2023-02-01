38b9246c085ba66ce746d7d465892084b0939e45
Alekxos <mckeehan@stanford.edu>
2023-02-01 13:05:01 -0800

    Make max capacitance infinite for VeryLargeCap

--- SQcircuit/elements.py
+++ SQcircuit/elements.py
@@ -232,7 +232,7 @@ class VerySmallCap(Capacitor):
 class VeryLargeCap(Capacitor):
 
     def __init__(self):
-        super().__init__(1e20, "F", Q=None)
+        super().__init__(1e20, "F", Q=None, max_value=float('inf'))
 
 
 class Inductor(Element):
