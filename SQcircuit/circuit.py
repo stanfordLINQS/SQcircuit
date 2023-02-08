Fix MAJOR ERROR with NORMALIZATION of JUNCTION

SQcircuit/circuit.py
@@ -2124,10 +2124,16 @@ class Circuit:
                     # max_tensor = sqf.cast(element.max_value, dtype=torch.float, requires_grad=True)
                     if element._value < element.min_value:
                         raise_value_out_of_bounds_warning(element.min_value, element._value.detach().numpy())
-                        element.set_value(element.min_value)
+                        if type(element) is Junction:
+                            element.set_value(element.min_value / 2 / np.pi)
+                        else:
+                            element.set_value(element.min_value)
                     if element._value > element.max_value:
                         raise_value_out_of_bounds_warning(element.max_value, element._value.detach().numpy())
-                        element.set_value(element.max_value)
+                        if type(element) is Junction:
+                            element.set_value(element.min_value / 2 / np.pi)
+                        else:
+                            element.set_value(element.max_value)
                     # element._value = sqf.maximum(element._value, min_tensor)
                     # element._value = sqf.minimum(element._value, max_tensor)
 