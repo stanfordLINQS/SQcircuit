b291797e202b9edf6510443358bb709015671596
Alekxos <mckeehan@stanford.edu>
2023-02-25 16:21:07 -0800

    Small fix to logs.py

--- SQcircuit/logs.py
+++ SQcircuit/logs.py
@@ -4,6 +4,7 @@ import warnings
 
 from SQcircuit.settings import get_optim_mode
 import SQcircuit.functions as sqf
+import numpy as np
 
 UNIT_ERROR = "The input unit is not correct. Look at the API documentation " \
              "for the correct input format."
@@ -30,8 +31,8 @@ def raise_value_out_of_bounds_error(element_type, cutoff_value, element_value):
 
 def raise_value_out_of_bounds_warning(element_type, cutoff_value, element_value):
     if cutoff_value < element_value:
-        overshoot_ratio = sqf.round((element_value - cutoff_value) / cutoff_value, 3)
+        overshoot_ratio = np.round((element_value - cutoff_value) / cutoff_value, 3)
         warnings.warn(f"Setting {element_value} for element of type {element_type} is {overshoot_ratio} times above the maximum allowed value of {cutoff_value}.")
     if cutoff_value > element_value:
-        undershoot_ratio = sqf.round((cutoff_value - element_value) / cutoff_value, 3)
+        undershoot_ratio = np.round((cutoff_value - element_value) / cutoff_value, 3)
         warnings.warn(f"Setting {element_value} for element of type {element_type} is {undershoot_ratio} times lower than the maximum allowed value of {cutoff_value}.")
